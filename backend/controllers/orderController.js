6b8fe16a3fb22559698be7a487175f7af17a679c
Ahamed Asiff <it21154980@my.sliit.lk>
2025-08-26 09:11:15 +0530

    Add user orders retrieval and fix item field name in order model

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -50,7 +50,17 @@ const allOrders = async (req, res) => {
 
 // user orders Data for frontend
 const userOrders = async (req, res) => {
-    
+    try {
+        const { userId } = req.body
+
+        const orders = await orderModel.find({ userId })
+        res.json({success : true, orders})
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({success : false, message: error.message}) 
+    }    
 }
 
 //update order status from Admin Panel
